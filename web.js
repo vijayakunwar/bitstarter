8805e33f20f42a5f56d4fd3b075dceb1b1735315
vijayakunwar <kunwar.vijaya@gmail.com>
2013-07-11 17:12:20 +0000

    working  web.js file to display content of index.html

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
-var express=require('express');
-
-var fs=require('fs');
-var app= express.createServer(express.logger());
-
-var content = fs.readFileSync("~/bitstarter/index.html","utf-8");
-
-var buffer= new Buffer(256);
-
-app.get('/',function(request,response){
-response.send(buffer.toString("utf-8",content));
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port,function(){
-console.log("Listening on"+port);
-});
+var fs = require("fs");
+	var fileName = "index.html";
+	fs.exists(fileName, function(exists) {
+	  if (exists) {
+	    fs.stat(fileName, function(error, stats) {
+	      fs.open(fileName, "r", function(error, fd) {
+	        var buffer = new Buffer(stats.size);
+	        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+ 
+          var data = buffer.toString("utf8", 0, buffer.length);
+	          console.log(data);
+	          fs.close(fd);
+	        });
+	      });
+	    });
+	  }
+	});
