4f23aeba12cdaa2fc5176aa6c3f4ef435a67d2f4
vijayakunwar <kunwar.vijaya@gmail.com>
2013-07-11 17:48:58 +0000

    web.js starting from hello world again

--- web.js
+++ web.js
@@ -1,17 +1,16 @@
-var fs = require("fs");
-	var fileName = "index.html";
-	fs.exists(fileName, function(exists) {
-	  if (exists) {
-	    fs.stat(fileName, function(error, stats) {
-	      fs.open(fileName, "r", function(error, fd) {
-	        var buffer = new Buffer(stats.size);
-	        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
- 
-          var data = buffer.toString("utf8", 0, buffer.length);
-	          console.log(data);
-	          fs.close(fd);
-	        });
-	      });
-	    });
-	  }
-	});
+
+
+
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
